f25fe34d912fa278cff609cbf039b56a11260f8f
Jesús Fco Cortés <jfcr@live.com>
2019-08-23 09:07:38 -0500

    Initing With Contact View

--- resources/views/visitor/index.blade.php
+++ resources/views/visitor/index.blade.php
@@ -73,43 +73,119 @@
             It’s easy. Just click “Edit Text” or double click me to add your own <br>content and make changes to the font. </p>
     </div>
     <div class="flex">
-        <div class="backgroundImage centrarFlex" style="background-image: url(img/index/quienesBackground1.jpg);">
-            <div>
-                <h3>NOMBRE DE TRABAJO</h3>
-                <p>Una breve descripción</p>
+
+        <div class="backgroundImage centrarFlex" style="background-image: url(img/index/trabajos.jpg);">
+            <div class="expandFatherContainerSize centrarFlex paddingDiv">
+                <div class="">
+                    
+                    <h3 class="text_center">NOMBRE DE TRABAJO</h3>
+                    <p class="text_center">Una breve descripción</p>
+                    
+                </div>
             </div>
         </div>
-        <div class="backgroundImage centrarFlex" style="background-image: url(img/index/quienesBackground1.jpg);">
-            <div>
-                <h3>NOMBRE DE TRABAJO</h3>
-                <p>Una breve descripción</p>
+        <div class="backgroundImage centrarFlex" style="background-image: url(img/index/trabajos.jpg);">
+            <div class="expandFatherContainerSize centrarFlex paddingDiv">
+                <div class="">
+                    
+                    <h3 class="text_center">NOMBRE DE TRABAJO</h3>
+                    <p class="text_center">Una breve descripción</p>
+                    
+                </div>
             </div>
         </div>
-        <div class="backgroundImage centrarFlex" style="background-image: url(img/index/quienesBackground1.jpg);">
-            <div>
-                <h3>NOMBRE DE TRABAJO</h3>
-                <p>Una breve descripción</p>
+        <div class="backgroundImage centrarFlex" style="background-image: url(img/index/trabajos.jpg);">
+            <div class="expandFatherContainerSize centrarFlex paddingDiv">
+                <div class="">
+                    
+                    <h3 class="text_center">NOMBRE DE TRABAJO</h3>
+                    <p class="text_center">Una breve descripción</p>
+                    
+                </div>
             </div>
         </div>
-        <div class="backgroundImage centrarFlex" style="background-image: url(img/index/quienesBackground1.jpg);">
-            <div>
-                <h3>NOMBRE DE TRABAJO</h3>
-                <p>Una breve descripción</p>
+        <div class="backgroundImage centrarFlex" style="background-image: url(img/index/trabajos.jpg);">
+            <div class="expandFatherContainerSize centrarFlex paddingDiv">
+                <div class="">
+                    
+                    <h3 class="text_center">NOMBRE DE TRABAJO</h3>
+                    <p class="text_center">Una breve descripción</p>
+                    
+                </div>
             </div>
         </div>
-        <div class="backgroundImage centrarFlex" style="background-image: url(img/index/quienesBackground1.jpg);">
-            <div>
-                <h3>NOMBRE DE TRABAJO</h3>
-                <p>Una breve descripción</p>
+        <div class="backgroundImage centrarFlex" style="background-image: url(img/index/trabajos.jpg);">
+            <div class="expandFatherContainerSize centrarFlex paddingDiv">
+                <div class="">
+                    
+                    <h3 class="text_center">NOMBRE DE TRABAJO</h3>
+                    <p class="text_center">Una breve descripción</p>
+                    
+                </div>
             </div>
         </div>
-        <div class="backgroundImage centrarFlex" style="background-image: url(img/index/quienesBackground1.jpg);">
-            <div>
-                <h3>NOMBRE DE TRABAJO</h3>
-                <p>Una breve descripción</p>
+        <div class="backgroundImage centrarFlex" style="background-image: url(img/index/trabajos.jpg);">
+            <div class="expandFatherContainerSize centrarFlex paddingDiv">
+                <div class="">
+                    
+                    <h3 class="text_center">NOMBRE DE TRABAJO</h3>
+                    <p class="text_center">Una breve descripción</p>
+                    
+                </div>
             </div>
         </div>
     </div>
+</section>
+
+<section id="clients">
+        <div class="container2">
+            <div class="line_title"></div>
+            <h1 class="title">NUESTROS <br>CLIENTES</h1>            
+            <p>I'm a paragraph. Click here to add your own text and edit me.<br>
+                It’s easy. Just click “Edit Text” or double click me to add your own <br>content and make changes to the font. </p>
+
+            <div class="flex ">
+                <div><img src="img/clientes/1.webp"></div>
+                <div><img src="img/clientes/2.webp"></div>
+                <div><img src="img/clientes/3.webp"></div>
+                <div><img src="img/clientes/4.webp"></div>
+                <div><img src="img/clientes/5.webp"></div>
+                <div><img src="img/clientes/6.webp"></div>
+                <div><img src="img/clientes/7.webp"></div>
+                <div><img src="img/clientes/8.webp"></div>
+            </div>
+        </div>
+    
+</section>
+
+<section id="contact">
+        <div class="container2">
+            <div class="line_title"></div>
+            <h1 class="title">CONTACTO</h1>            
+            
+            <div class="flex">
+                <div>
+
+                    <form>
+                        <input type="text" name="name" required>
+                        <input type="email" name="email" required>
+                        <input type="phone" name="phone" required>
+                        <textarea required><textarea>
+                        <button>Enviar</button>
+                    </form>
+
+                </div>
+
+                <div>
+                    <h5>Estamos encantado de atenderte</h5>
+                    <p>
+                        <a href="">correo@madproducciones.com</a><br>
+                        <a href="">961-123-4567</a>
+                    </p>
+                </div>
+
+            </div>
+    
 </section>
 
 @endsection
